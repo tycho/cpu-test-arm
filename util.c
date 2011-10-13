7a084117a1c36ba774491dbafe73137023bdcdb6
Steven Noonan <steven@uplinklabs.net>
2011-10-12 23:58:48 -0700

    make tm_ticks static inline
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- util.c
+++ util.c
@@ -1,20 +1,4 @@
-#include <stdint.h>
-#include <time.h>
-#include <unistd.h>
-
 #include "util.h"
-
-/* ticks in microseconds */
-uint32_t tm_ticks(void)
-{
-    struct timespec t;
-#ifdef _POSIX_MONOTONIC_CLOCK
-    clock_gettime(CLOCK_MONOTONIC, &t);
-#else
-    clock_gettime(CLOCK_REALTIME, &t);
-#endif
-    return 1000000LL * (uint64_t)t.tv_sec + (uint64_t)(t.tv_nsec / 1000);
-}
 
 /* in microseconds */
 uint32_t tm_resolution(void)
--- util.h
+++ util.h
@@ -1,7 +1,22 @@
 #ifndef __included_util_h
 #define __included_util_h
 
-uint32_t tm_ticks(void);
+#include <stdint.h>
+#include <time.h>
+#include <unistd.h>
+
+/* ticks in microseconds */
+static inline uint32_t tm_ticks(void)
+{
+	struct timespec t;
+#ifdef _POSIX_MONOTONIC_CLOCK
+	clock_gettime(CLOCK_MONOTONIC, &t);
+#else
+	clock_gettime(CLOCK_REALTIME, &t);
+#endif
+	return 1000000LL * (uint64_t)t.tv_sec + (uint64_t)(t.tv_nsec / 1000);
+}
+
 uint32_t tm_resolution(void);
 
 #endif
